e16fb6f41d50fb56e257a13231c63221f2b95d57
arraxx1986 <154022029+arraxx1986@users.noreply.github.com>
2024-06-21 00:40:46 +0300

    Update Zolotykh_D_S_BelayVE_5_1.py

--- lesson 5/Zolotykh_D_S_BelayVE_5_1.py
+++ lesson 5/Zolotykh_D_S_BelayVE_5_1.py
@@ -1,1 +1,6 @@
-
+matrix = [[1,2,3],[4,5,6],[7,8,9]]
+s = 0
+for i in range(len(matrix)):
+    for k in range(len(matrix)-2):
+        s = s + (matrix[i][k]*matrix[i][k+1]*matrix[i][k+2])*len(matrix[k])
+print(s*len(matrix))
