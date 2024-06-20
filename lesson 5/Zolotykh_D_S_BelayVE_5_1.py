a2438810f27a163dbce9b882a177314bcce2a646
arraxx1986 <154022029+arraxx1986@users.noreply.github.com>
2024-06-21 01:08:40 +0300

    Delete lesson 5/Zolotykh_D_S_BelayVE_5_1.py

--- lesson 5/Zolotykh_D_S_BelayVE_5_1.py
+++
@@ -1,6 +0,0 @@
-matrix = [[1,2,3],[4,5,6],[7,8,9]]
-s = 0
-for i in range(len(matrix)):
-    for k in range(len(matrix)-2):
-        s = s + (matrix[i][k]*matrix[i][k+1]*matrix[i][k+2])*len(matrix[i])
-print(s*len(matrix))
